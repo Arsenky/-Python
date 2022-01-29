54d49ddbf9f154d8e2ee338222aec240334cee52
Arsenky <arsenky@yandex.ru>
2022-01-29 10:23:50 +0300

    Lesson6 corrected

--- pythonProject/Lesson6/Task3.py
+++ pythonProject/Lesson6/Task3.py
@@ -2,8 +2,8 @@ import json
 import sys
 
 result_dict = {}
-with open('users.txt', 'r', encoding='utf-8') as users:
-    with open('hobby.txt', 'r', encoding='utf-8') as hobbys:
+with open('users.csv', 'r', encoding='utf-8') as users:
+    with open('hobby.csv', 'r', encoding='utf-8') as hobbys:
         for name in users:
             result_dict[name.strip()] = hobbys.readline().strip()
             if not result_dict[name.strip()]:
